8c80ef9a61ad5224a57b54bfb535a016d4a2a6bf
m1050728 <m1050728@A2ML45175M.local>
2022-01-06 01:21:12 +0530

    Initial project commit

---
+++ Wip_AssignmentTests/Wip_AssignmentTests.swift
@@ -0,0 +1,33 @@
+//
+//  Wip_AssignmentTests.swift
+//  Wip_AssignmentTests
+//
+//  Created by m1050728 on 06/01/22.
+//
+
+import XCTest
+@testable import Wip_Assignment
+
+class Wip_AssignmentTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
